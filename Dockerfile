f5351ff54e78b2b575ee1ab9dd41a6fa882d682e
Spirit1998HD <21293774+Spirit1998HD@users.noreply.github.com>
2026-01-14 03:45:52 +0100

    add unzip and bash

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM eclipse-temurin:25-jre
 
+RUN apk add --no-cache bash unzip
+
 RUN mkdir -p /hytale && \
     mkdir -p /hytale/auth && \
     mkdir -p /hytale/server
--- resources/init.sh
+++ resources/init.sh
@@ -4,7 +4,7 @@
 
 cd /hytale/server
 rm -rf ./hytale-downloader
-wget -q -O hytale-downloader.zip https://downloader.hytale.com/hytale-downloader.zip
+curl -fsSL -o hytale-downloader.zip https://downloader.hytale.com/hytale-downloader.zip
 unzip -j hytale-downloader.zip hytale-downloader-linux-amd64 -d .
 mv ./hytale-downloader-linux-amd64 ./hytale-downloader
 chmod u+x ./hytale-downloader
