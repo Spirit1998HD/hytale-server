ab9b6fb445275fc065cdbb5e604b88c73a7266cb
Spirit1998HD <21293774+Spirit1998HD@users.noreply.github.com>
2026-01-14 04:00:16 +0100

    mute unzip output

--- resources/init.sh
+++ resources/init.sh
@@ -5,7 +5,7 @@
 cd /hytale/server
 rm -rf ./hytale-downloader
 curl -fsSL -o hytale-downloader.zip https://downloader.hytale.com/hytale-downloader.zip
-unzip -j hytale-downloader.zip hytale-downloader-linux-amd64 -d .
+unzip -qq -j hytale-downloader.zip hytale-downloader-linux-amd64 -d .
 mv ./hytale-downloader-linux-amd64 ./hytale-downloader
 chmod u+x ./hytale-downloader
 rm -rf hytale-downloader.zip
